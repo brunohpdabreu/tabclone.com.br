127c8a92fb79044a27a5658fb1d874d494117403
Bruno Abreu <brunohdabreu@gmail.com>
2025-04-19 19:53:49 +0000

    chore: adiciona a estrutura do componente `Home`
    Como requisito para levantar o serviço web (local), disponibilizado pelo
    Next.js, é necessário pelo menos um aquivo index.js na raiz do diretório
    pages.
    
    Então, foi adicionada a estrutura base, e mínima necessária, pra expor o
    componente Home e, com isso, conseguir levantar com sucesso o serviço.

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <blockquote>"Se você quiser fazer uma torta de maçã a partir do zero, você deve, primeiro, inventar o universo." Carl Sagan</blockquote>
+}
+
+export default Home;
